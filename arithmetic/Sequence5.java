f2b5c46d08aec19568652706e1ff66eeb499f310
gt1854 <gt1854@naver.com>
2018-12-11 16:41:52 +0900

    first commit

---
+++ arithmetic/Sequence5.java
@@ -0,0 +1,14 @@
+package arithmetic;
+/**
+ 등차수열 : 연속하는 두 항의 차이가 모두 일정한 수열
+ 공차(common difference): 공통적으로 나타나는 차이
+ * */
+public class Sequence5 {
+	public static void main(String[] args) {
+		// 2+8+14+20+26 = 70
+		
+
+		
+		
+	}
+}
